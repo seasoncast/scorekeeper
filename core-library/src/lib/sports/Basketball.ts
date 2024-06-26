3e23ebd684adad4154152595475122624cc80070
Maxwell Smith <themaxsmith@icloud.com>
2024-06-26 15:02:44 -0700

    Implemented basketball shot tracking with missed and made shots.

--- core-library/src/lib/sports/Basketball.ts
+++ core-library/src/lib/sports/Basketball.ts
@@ -1,13 +1,18 @@
+import { Player, Position, Team } from '../../types';
 import Editor, { IAction } from '../Editor';
 
 export default class Basketball extends Editor {
   actions: { [key: string]: IAction } = {
-    BASKETBALL_SHOT_MISSED: {
+    SHOT_MISSED: {
+      name: 'SHOT_MISSED',
       description: '2 point shot missed',
       args: [
         { id: 'teamShooting', type: 'Team' },
+        { id: 'teamDefending', type: 'Team' },
         { id: 'position', type: 'Position', optional: true },
         { id: 'shooter', type: 'Player', optional: true },
+        { id: 'was_blocked', type: 'boolean', optional: true },
+        { id: 'blocked_by', type: 'Player', optional: true },
       ],
       result: [
         {
@@ -17,8 +22,8 @@ export default class Basketball extends Editor {
           player_id: '$shooter',
           team_id: '$teamShooting',
           data_change: {
-            shot_attempts: { type: 'increment', value: 1 },
-            shot_2_attempts: { type: 'increment', value: 1 },
+            shot_attempts: 1,
+            shot_2_attempts: 1,
             shot_position: {
               type: 'push',
               value: '$position',
@@ -28,10 +33,12 @@ export default class Basketball extends Editor {
         },
       ],
     },
-    BASKETBALL_SHOT_MADE: {
+    SHOT_MADE: {
+      name: 'SHOT_MADE',
       description: '2 point shot made',
       args: [
         { id: 'teamShooting', type: 'Team' },
+        { id: 'teamDefending', type: 'Team' },
         { id: 'position', type: 'Position', optional: true },
         { id: 'shooter', type: 'Player', optional: true },
       ],
@@ -43,11 +50,11 @@ export default class Basketball extends Editor {
           player_id: '$shooter',
           team_id: '$teamShooting',
           data_change: {
-            shot_attempts: { type: 'increment', value: 1 },
-            shot_made: { type: 'increment', value: 1 },
-            shot_2_attempts: { type: 'increment', value: 1 },
-            shot_2_made: { type: 'increment', value: 1 },
-            points: { type: 'increment', value: 2 },
+            shot_attempts: 1,
+            shot_made: 1,
+            shot_2_attempts: 1,
+            shot_2_made: 1,
+            points: 2,
             shot_position: {
               type: 'push',
               value: '$position',
@@ -60,19 +67,26 @@ export default class Basketball extends Editor {
   };
 
   shotMissed(args: {
-    teamShooting: string;
-    teamDefending: string;
-    position?: [number, number];
-    shooter?: string;
+    teamShooting: Team;
+    teamDefending: Team;
+    position?: Position;
+    shooter?: Player;
     was_blocked?: boolean;
-    blocked_by?: string;
+    blocked_by?: Player;
   }) {
     this.sportEvent.updateStats((stats) => {
-      return this.parseAction(
-        stats,
-        this.actions['BASKETBALL_SHOT_MISSED'],
-        args
-      );
+      return this.parseAction(stats, this.actions['SHOT_MISSED'], args);
+    });
+  }
+
+  shotMade(args: {
+    teamShooting: Team;
+    teamDefending: Team;
+    position?: Position;
+    shooter?: Player;
+  }) {
+    this.sportEvent.updateStats((stats) => {
+      return this.parseAction(stats, this.actions['SHOT_MADE'], args);
     });
   }
 }
