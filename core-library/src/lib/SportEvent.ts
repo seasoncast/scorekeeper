3ced5ca8ed77829a266913ed672c36e8d1deb240
Maxwell Smith <themaxsmith@icloud.com>
2025-01-17 13:37:09 -0800

    refactor: Update import path and format constructor in SportEvent

--- core-library/src/lib/SportEvent.ts
+++ core-library/src/lib/SportEvent.ts
@@ -1,8 +1,8 @@
+import { CollaborationClient } from '@org/client-library';
 import ISportEvent from '../types/ISportEvent';
 import IStats from '../types/IStats';
 import ITimelineEvent from '../types/ITimelineEvent';
 import Team from './Team';
-import { CollaborationClient } from '@collab/client';
 
 class SportEvent {
   public sport_data: ISportEvent;
@@ -20,7 +20,13 @@ class SportEvent {
     sport_type: string;
     collabClient?: CollaborationClient;
   });
-  public constructor({ id, collabClient }: { id: string; collabClient?: CollaborationClient });
+  public constructor({
+    id,
+    collabClient,
+  }: {
+    id: string;
+    collabClient?: CollaborationClient;
+  });
   constructor(args: any) {
     this.collabClient = args.collabClient;
     this.sport_data = {
@@ -36,7 +42,6 @@ class SportEvent {
         global_data: {},
         player_data: [],
       },
-
     };
   }
   public setCallbackChange(callback_change: (sport_data: ISportEvent) => void) {
@@ -64,19 +69,22 @@ class SportEvent {
   public updateStats(changer_callback: (stats: IStats) => ITimelineEvent) {
     const current_stats = JSON.parse(JSON.stringify(this.sport_data.stats));
     const timeline_event = changer_callback(current_stats);
-    
+
     // Update local state
     this.sport_data.stats = current_stats;
-    
+
     // If using collaboration, send the update
     if (this.collabClient) {
-      const diff = this.collabClient.compare(this.sport_data.stats, current_stats);
+      const diff = this.collabClient.compare(
+        this.sport_data.stats,
+        current_stats
+      );
       this.collabClient.pushTimeline(diff, {
         ...timeline_event,
-        type: 'stats_update'
+        type: 'stats_update',
       });
     }
-    
+
     this.callback_change?.(this.sport_data);
   }
 
